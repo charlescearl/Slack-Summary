d6d411076a525d2fb3d78489f76a799156475ba2
Charles Earl <charles.cearl@gmail.com>
2015-10-26 21:29:34 -0400

    Minor revisions to summarizer
    Still some improvements to work in the count matrix setup.
    Probably other features to focus on at this point.

--- lsa.py
+++ lsa.py
@@ -52,7 +52,6 @@ class LsaSummarizer(BaseSummarizer):
         # empty document
         if not dictionary:
             return ()
-
         matrix = self._create_matrix(dictionary)
         matrix = self._compute_term_frequency(matrix)
         u, sigma, v = singular_value_decomposition(matrix, full_matrices=False)
@@ -72,29 +71,11 @@ class LsaSummarizer(BaseSummarizer):
         words = [wd.orth_ for wd in self.nlp_doc if wd.pos != PUNCT]
         unique_words = frozenset(w.lemma_ for w in self.nlp_doc if w not in STOPWORDS and w.tag_ != "PRP" and (w.pos == VERB or w.pos == NOUN))
         logger.info("Have %s unique words" % len(unique_words))
-
         return dict((w, i) for i, w in enumerate(unique_words))
 
-
-    def retrieve_main_bow(tokens):
-        bow = set()
-        for chunk in tokens.noun_chunks:
-            if chunk.orth_.lower() not in stop_nouns and chunk.orth_.lower() not in stop_words:
-                bow.add(chunk.orth_.lower())
-                bow.add(chunk.root.head.orth_.lower())
-        for ent in tokens.ents:
-            bow.add(ent.orth_.lower())
-        for tok in tokens:
-            if tok.dep_ == 'advcl':
-                bow.add(' '.join([ti.orth_.lower() for ti in list(tok.children) if ti.pos != PRON and ti.orth_.lower() not in stop_words]))
-                bow.add(tok.orth_.lower())
-            if tok.pos == PROPN and tok.orth_.lower() not in STOPWORDS:
-                bow.add(tok.orth_.lower())
-        mt = re.sub(r'[\n\t\n]', u'', u' '.join(list(bow))+u'.')
-        return mt if len(mt.strip().split()) > 2 else None
-
-    def collect_bow(txt):
-        return [st for st in [retrieve_main_bow(s.text.strip()) for s in nlp(txt).sents if len(s) > 2] if st]
+    def collect_bow(self, txt):
+        sents = nlp(txt).sents
+        return [x for x in [retrieve_main_bow(sent) for sent in sents] if x]
 
     def _create_matrix(self, dictionary):
         """
@@ -111,16 +92,11 @@ class LsaSummarizer(BaseSummarizer):
                 "LSA algorithm may not work properly."
             )
             logger.warn(message % (words_count, sentences_count))
-
         # create matrix |unique words|×|sentences| filled with zeroes
         matrix = numpy.zeros((words_count, sentences_count))
         for col, sentence in enumerate(sentences):
-            for word in [wd.lemma_ for wd in sentence if wd.pos != PUNCT]:
-                # only valid words is counted (not stop-words, ...)
-                if word in dictionary:
-                    row = dictionary[word]
-                    matrix[row, col] += 1
-
+            for word in [wd.lemma_ for wd in sentence if wd.lemma_ in dictionary]:
+                matrix[dictionary[word], col] += 1
         return matrix
 
     def _compute_term_frequency(self, matrix, smooth=0.4):
@@ -139,7 +115,6 @@ class LsaSummarizer(BaseSummarizer):
                 if max_word_frequency != 0:
                     frequency = matrix[row, col]/max_word_frequency
                     matrix[row, col] = smooth + (1.0 - smooth)*frequency
-
         return matrix
 
     def _compute_ranks(self, sigma, v_matrix):
@@ -149,11 +124,22 @@ class LsaSummarizer(BaseSummarizer):
             int(len(sigma)*LsaSummarizer.REDUCTION_RATIO))
         powered_sigma = tuple(s**2 if i < dimensions else 0.0
             for i, s in enumerate(sigma))
-
         ranks = []
         # iterate over columns of matrix (rows of transposed matrix)
         for column_vector in v_matrix.T:
             rank = sum(s*v**2 for s, v in zip(powered_sigma, column_vector))
             ranks.append(math.sqrt(rank))
-
         return ranks
+
+def retrieve_main_bow(tokens):
+    bow = set()
+    for tok in tokens:
+        if tok.pos != PUNCT:
+            if tok.dep_ == 'advcl' or tok.dep == 'xcomp':
+                bow.add(' '.join([ti.lower_ for ti in list(tok.children) if tok.tag_ != "PRP" and ti.lower_ not in STOPWORDS]))
+                bow.add(tok.lower_)
+            if tok.pos == NOUN or tok.pos == VERB:
+                if tok.tag_ != "PRP" and tok.lower_ not in STOPWORDS:
+                    bow.add(tok.lower_)
+    mt = re.sub(r'[\n\t\n]', u'', u' '.join(list(bow))+u'.')
+    return mt if len(mt.strip().split()) > 2 else None
