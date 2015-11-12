fd712b97d5ae79a7f94b42b481f3eff4c291f9a9
Charles Earl <charles.cearl@gmail.com>
2015-11-12 01:34:41 -0500

    Modify log file paths

--- ts_config.py
+++ ts_config.py
@@ -1,8 +1,8 @@
 SUMMARY_INTERVALS = [{'days': 5, 'size': 2}, ]
 TS_DEBUG = True
-TS_LOG = "./ts_summ.log"
+TS_LOG = "ts_summ.log"
 DEBUG=True
-LOG_FILE="./summary.log"
+LOG_FILE="summary.log"
 TEST_JSON="./data/test-events-elastic.json"
 SUMMS=["spacy"]
 
